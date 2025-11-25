b0260a8b7c7dcfcb0e5f8ef283a1092b0565609b
Anurag Biswas Koushik <anurag.koushik69@gmail.com>
2025-11-25 13:17:31 -0700

    Overhaul

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ExpandableCardDemo } from './components/ExpandableCard'
+
+function App() {
+  return (
+    <div className="projects-container" style={{ width: '100%', minHeight: '200px' }}>
+      <ExpandableCardDemo />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/hooks/use-outside-click.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+export function useOutsideClick(ref, handler) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        handler();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, handler]);
+}
+
